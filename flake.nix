5383b8fc069f57b38fa755d4ba56d19c50191142
Rob Harbaugh <rob@rharbaugh.com>
2025-07-31 11:35:41 -0400

    updating flake to include augustine

--- flake.nix
+++ flake.nix
@@ -16,6 +16,29 @@
 		...
 	}: {
 		nixosConfigurations = {
+			augustine = let
+				username = "rob";
+				specialArgs = {inherit username;};
+			in
+				nixpkgs.lib.nixosSystem {
+					inherit specialArgs;
+					system = "x86_64-linux";
+
+					modules = [
+						./hosts/augustine
+						./users/${username}/nixos.nix
+
+						home-manager.nixosModules.home-manager
+						{
+							home-manager.useGlobalPkgs = true;
+							home-manager.useUserPackages = true;
+
+							home-manager.extraSpecialArgs = inputs // specialArgs;
+							home-manager.users.${username} = import ./users/${username}/home.nix;
+						}
+					];
+				};
+			};
 			benedict = let
 				username = "rob";
 				specialArgs = {inherit username;};
